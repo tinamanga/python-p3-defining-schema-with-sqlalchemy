c01e97c54cdb99290dc3a2f9de154c0b783cfd85
tinamanga <tinamanga@gmail.com>
2025-05-23 13:58:03 +0300

    using an external library to simplfy tasks from earlier ORM lessons

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -5,8 +5,14 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+# defining the Student class with SQLAlchemy
 class Student(Base):
-    pass
-
+     __tablename__ = 'students'
+# creating columns (2 of them)
+     id = Column(Integer(), primary_key=True)
+     name = Column(String())
+# Setting up the create_engine and call create_all() to create the students table in a database (students.db).
 if __name__ == '__main__':
-    pass
+     engine = create_engine('sqlite:///students.db')
+     Base.metadata.create_all(engine)
+   
